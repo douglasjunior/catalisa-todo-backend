f868481aefe8cfba5b661679a76c5ea89977cd99
Douglas Nassif Roma Junior <nassifrroma@gmail.com>
2022-04-23 17:35:37 -0300

    configura banco de dados oracle

--- modelos.js
+++ modelos.js
@@ -1,46 +1,124 @@
 const createKnex = require('knex');
+const oracledb = require('oracledb');
+const crypto = require('crypto');
+
+const NAME_ALREAD_USED_ERROR_CODE = 955;
+
+const {
+  DB_USER,
+  DB_PASSWORD,
+  DB_DATABASE,
+  DB_CONNECT_STRING,
+  NODE_ENV,
+} = process.env;
+
+const DEBUG = NODE_ENV === 'development';
+
+console.log({
+  DB_USER,
+  DB_PASSWORD,
+  DB_DATABASE,
+  DB_CONNECT_STRING,
+  NODE_ENV,
+  DEBUG
+})
+
+oracledb.initOracleClient({ libDir: 'C:\\Users\\dougl\\Downloads\\instantclient-basic-windows.x64-21.3.0.0.0\\instantclient_21_3' });
 
 const knex = createKnex({
-  client: 'sqlite3',
-  debug: true,
-  useNullAsDefault: true,
+  client: 'oracledb',
+  debug: DEBUG,
   connection: {
-    filename: './database/catalisa-todo.sqlite3',
-  }
+    // host: string,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+    // domain: string,
+    // instanceName: 'DB202204222106',
+    debug: DEBUG,
+    // requestTimeout: number,
+    connectString: DB_CONNECT_STRING,
+    // expirationChecker(): boolean,
+  },
+  wrapIdentifier: (value, origImpl, queryContext) => value, // origImpl(convertToSnakeCase(value))
 });
 const bookshelf = require('bookshelf')(knex);
 
 const Usuario = bookshelf.model('Usuario', {
+  idAttribute: 'ID',
   tableName: 'usuarios',
   requireFetch: false,
   hidden: [
-    'senha'
+    'SENHA'
   ],
+  initialize: function initialize() {
+    this.constructor.__super__.initialize.apply(this, arguments);
+    // Your initialization code ...
+
+    this.on('saving', (model, attrs, options) => {
+      if (!this.get('ID')) {
+        this.set('ID', crypto.randomUUID());
+      }
+    });
+  }
 });
 
 const Tarefa = bookshelf.model('Tarefa', {
+  idAttribute: 'ID',
   tableName: 'tarefas',
   requireFetch: false,
+  initialize: function initialize() {
+    this.constructor.__super__.initialize.apply(this, arguments);
+    // Your initialization code ...
+
+    this.on('saving', (model, attrs, options) => {
+      if (!this.get('ID')) {
+        this.set('ID', crypto.randomUUID());
+      }
+    });
+  }
 });
 
 exports.connectDatabase = async () => {
-  await knex.raw(`
-    CREATE TABLE IF NOT EXISTS usuarios (
-      id    INTEGER       PRIMARY KEY AUTOINCREMENT NOT NULL,
-      nome  VARCHAR (200) NOT NULL,
-      email VARCHAR (200) NOT NULL,
-      senha VARCHAR (80)  NOT NULL
-    );
-  `);
-  await knex.raw(`
-    CREATE TABLE IF NOT EXISTS tarefas (
-      id           INTEGER        PRIMARY KEY AUTOINCREMENT NOT NULL,
-      titulo       VARCHAR (300)  NOT NULL,
-      data_criacao DATETIME       NOT NULL,
-      concluida    BOOLEAN        NOT NULL,
-      usuario_id   INTEGER        REFERENCES usuarios (id) NOT NULL
-    );
-  `);
+  try {
+    // await knex.raw('DROP TABLE usuarios CASCADE CONSTRAINTS');
+    await knex.raw(`
+      CREATE TABLE usuarios (
+        id    VARCHAR (40)    PRIMARY KEY NOT NULL,
+        nome  VARCHAR (200)   NOT NULL,
+        email VARCHAR (200)   NOT NULL,
+        senha VARCHAR (80)    NOT NULL
+      )
+    `);
+  } catch (err) {
+    // ORA-00955: name is already used by an existing object
+    if (err.errorNum !== NAME_ALREAD_USED_ERROR_CODE) {
+      throw err;
+    }
+  }
+  try {
+    // await knex.raw('DROP TABLE tarefas CASCADE CONSTRAINTS');
+    await knex.raw(`
+      CREATE TABLE tarefas (
+        id           VARCHAR (40)     PRIMARY KEY NOT NULL,
+        titulo       VARCHAR (300)    NOT NULL,
+        data_criacao TIMESTAMP        NOT NULL,
+        concluida    char(1)          NOT NULL,
+        usuario_id   VARCHAR (40)     NOT NULL,
+        CONSTRAINT fk_usuario_id
+          FOREIGN KEY (usuario_id)
+          REFERENCES usuarios (id)
+      )
+    `);
+  } catch (err) {
+    // ORA-00955: name is already used by an existing object
+    if (err.errorNum !== NAME_ALREAD_USED_ERROR_CODE) {
+      throw err;
+    }
+  }
+
+  const tables = await knex.raw('select table_name from user_tables order by 1');
+  console.log({ tables })
 };
 
 exports.Usuario = Usuario;
--- routes/users.js
+++ routes/users.js
@@ -25,25 +25,29 @@ function validacaoCadastro(request, response, next) {
 }
 
 router.post('/', validacaoCadastro, async function (req, res, next) {
-  // SELECT * FROM usuarios WHERE email = 'edvaldoszy@gmail.com'
-  const usuarioExistente = await modelos.Usuario
-    .where('email', '=', req.body.email)
-    .fetch();
-  if (usuarioExistente) {
-    res.status(400).json({
-      mensagem: 'O endereço de e-mail já está cadastrado'
+  try {
+    // SELECT * FROM usuarios WHERE email = 'edvaldoszy@gmail.com'
+    const usuarioExistente = await modelos.Usuario
+      .where('EMAIL', '=', req.body.email)
+      .fetch();
+    if (usuarioExistente) {
+      res.status(400).json({
+        mensagem: 'O endereço de e-mail já está cadastrado'
+      });
+      return;
+    }
+
+    const usuario = new modelos.Usuario({
+      NOME: req.body.nome,
+      EMAIL: req.body.email,
+      SENHA: criptografia.geraHash(req.body.senha),
     });
-    return;
+
+    const retorno = await usuario.save();
+    res.status(201).json(retorno);
+  } catch (err) {
+    next(err);
   }
-
-  const usuario = new modelos.Usuario({
-    nome: req.body.nome,
-    email: req.body.email,
-    senha: criptografia.geraHash(req.body.senha),
-  });
-
-  const retorno = await usuario.save();
-  res.status(201).json(retorno);
 });
 
 function validaLogin(req, res, next) {
@@ -61,11 +65,11 @@ function validaLogin(req, res, next) {
 
 router.post('/login', validaLogin, async function (req, res) {
   const usuarioExistente = await modelos.Usuario
-    .where('email', '=', req.body.email)
+    .where('EMAIL', '=', req.body.email)
     .fetch();
 
   if (usuarioExistente) {
-    const senhaEstaCorreta = criptografia.comparaHash(req.body.senha, usuarioExistente.get('senha'))
+    const senhaEstaCorreta = criptografia.comparaHash(req.body.senha, usuarioExistente.get('SENHA'))
     if (senhaEstaCorreta) {
       const token = jwt.geraToken(usuarioExistente);
       res.json({
